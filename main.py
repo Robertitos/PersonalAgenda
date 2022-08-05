90a9fb7475cac6507acc938f27c1aeddec9dc4ce
Robert <inyorfaceyt@gmail.com>
2022-08-05 10:44:12 +0200

    He creado la clase Backend donde he hecho un reloj que se actualiza

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 import sys
 import os
+import threading
+from time import sleep, strftime, localtime
 
 from PyQt6.QtGui import QGuiApplication
 from PyQt6.QtQml import QQmlApplicationEngine
 from PyQt6.QtQuick import QQuickWindow
+from PyQt6.QtCore import QObject, pyqtSignal
+
+class Backend(QObject):
+
+    def __init__(self):
+        QObject.__init__(self)
+
+    updated = pyqtSignal(str, arguments=['updater'])
+
+    def updater(self, curr_time):
+        self.updated.emit(curr_time)
+
+    def bootUp(self):
+        t_thread = threading.Thread(target=self._bootUp)
+        t_thread.daemon = True
+        t_thread.start()
+
+    def _bootUp(self):
+        while True:
+            curr_time = strftime("%H:%M:%S", localtime())
+            self.updater(curr_time)
+            sleep(0.1)
 
 QQuickWindow.setSceneGraphBackend('software')
 
@@ -12,5 +36,9 @@ app = QGuiApplication(sys.argv)
 engine = QQmlApplicationEngine()
 engine.quit.connect(app.quit)
 engine.load('./UI/main.qml')
+back_end = Backend()
+engine.rootObjects()[0].setProperty('backend', back_end)
+
+back_end.bootUp()
 
 sys.exit(app.exec())
